38a46bf332e97f436a39eba5827d65a8f149392c
toxiclight1790 <62153725+toxiclight1790@users.noreply.github.com>
2024-08-29 18:56:31 +0300

    create second project folder

---
+++ ProjectWithPicture/ProjectWithPicture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectWithPicture
+//
+//  Created by Серов Савелий on 29.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectWithPicture/ProjectWithPicture/ProjectWithPictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectWithPictureApp.swift
+//  ProjectWithPicture
+//
+//  Created by Серов Савелий on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectWithPictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
